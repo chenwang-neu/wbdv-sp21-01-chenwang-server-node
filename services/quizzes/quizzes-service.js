d436ec53bc21543cbf31b0b87d70c015232378eb
chen wang <wangchen.neu@gmail.com>
2021-04-11 16:38:52 -0400

    A7 server

---
+++ services/quizzes/quizzes-service.js
@@ -0,0 +1,24 @@
+const quizzes = require('./quizzes.json')
+
+// TODO: Node.js Assignment this week
+const findAllQuizzes = () => {
+    return quizzes
+}
+const findQuizById = (quizId) => {
+    return quizzes.find((quiz) => {
+        return quiz._id === quizId
+    })
+}
+
+// TODO: MongoDB Assignment next week
+const createQuiz = () => {}
+const updateQuiz = () => {}
+const deleteQuiz = () => {}
+
+module.exports = {
+    createQuiz,
+    findQuizById,
+    findAllQuizzes,
+    updateQuiz,
+    deleteQuiz,
+}
