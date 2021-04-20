f9c76471f6d089a9cd641a99e44ccb0f09cee410
chen wang <wangchen.neu@gmail.com>
2021-04-19 22:05:13 -0400

    deploy to heroku

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ require('./controllers/quizzes-controller')(app)
 require('./controllers/questions-controller')(app)
 require('./controllers/quiz-attempts-controller')(app)
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
